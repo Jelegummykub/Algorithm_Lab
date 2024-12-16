8651a66e6345fb9f5a27bcbdd8399b592dba43a2
jelegummykub <jelegummy.jrs@gmail.com>
2024-12-16 23:08:32 +0700

    Lab

---
+++ week1/kor8.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int n = 0;
+    cin >> n;
+    int A[n];
+
+    for(int i = 0 ; i < n ; i++){
+        cin >> A[i];
+    }
+
+    int tempmax = 0;
+    int s = 0;
+    int e = 0;
+
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < n - i ; j++){
+            int sum = 0;
+
+            for(int k = 0 ; k <= i ; k++){
+                sum += A[j + k];
+            }
+
+            if(sum > tempmax){
+                tempmax = sum;
+                s = j;
+                e = j + i;
+            }
+        }
+    }
+
+    for(int i = s ; i <= e ; i++){
+        cout << A[i] << ", ";
+    }
+
+    cout << "= " << tempmax << endl;
+
+}
